20955120a39ef0a039ff12d7b73c02617e677761
MCYJ <june1012june@gmail.com>
2021-10-14 04:22:29 +0900

    Create 23082.py

---
+++ 23082.py
@@ -0,0 +1,16 @@
+#author madaniel47
+n = int(input())
+result = []
+if n == 0:
+    print(n)
+else:
+    while n != 0:
+	    if n%3 == 2:
+		    result.append('T')
+		    n = n//3 + 1
+	    else:
+		    result.append(n%3)
+		    n = n//3
+
+    for i in range(len(result)):
+	    print(result[len(result)-i-1], end="")
