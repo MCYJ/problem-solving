a4e38696a8fe32f0af1692ed2c2884c0c51260fb
MCYJ <june1012june@gmail.com>
2022-05-03 00:33:31 +0900

    feat : boj 1302

---
+++ 1302.py
@@ -0,0 +1,26 @@
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(input())
+
+result = {}
+
+for i in range(n):
+    if arr[i] in result:
+        result[arr[i]] += 1
+    else:
+        result[arr[i]] = 1
+top = 0
+keys = result.keys()
+most = []
+for name in keys:
+    if top < result[name]:
+        top = result[name]
+        most = [name]
+    elif top == result[name]:
+        most.append(name)
+most.sort()
+
+for name in most:
+    print(name)
